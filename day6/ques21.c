59c0abd9781a1a179179f9aa8ced4d9aff9309b5
sonit-hue <sonitwadhwani306@gmail.com>
2026-06-04 23:39:55 +0530

    day6 uploaded

---
+++ day6/ques21.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num, binary[32], i = 0, j;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+
+    if(num == 0) {
+        printf("Binary = 0");
+        return 0;
+    }
+
+    while(num > 0) {
+        binary[i] = num % 2;
+        num /= 2;
+        i++;
+    }
+
+    printf("Binary = ");
+    for(j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+
+    return 0;
+}
